3862e9eadb64992dc7d85a1639a1ced12be82b50
Joe Mohrbacher <jhmohrbacher@gmail.com>
2019-10-28 16:02:21 -0500

    added a test for Users to test sonarcloud

---
+++ Project2JAGV.ObjectLogic/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2JAGV.ObjectLogic
+{
+    public class Users
+    {
+        private int _id { get; set; }
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("User Id can not be les than 0");
+                }
+
+                _id = value;
+            }
+        }
+    }
+}
---
+++ Project2JAGV.Test/UserTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+using Project2JAGV.ObjectLogic;
+
+namespace Project2JAGV.Test
+{
+    public class UserTest
+    {
+        [Fact]
+        public void User_Test()
+        {
+            Assert.Throws<ArgumentException>(() => new Users { Id = -1 });
+        }
+    }
+}
