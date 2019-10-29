36b1e0d15bb34cc697d0124e2398d64d631a342b
Joe Mohrbacher <jhmohrbacher@gmail.com>
2019-10-29 09:27:57 -0500

    added nuget packages for entity framworks

---
+++ Project2JAGV.DataAccess/Entities/Project2JAGVContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Project2JAGV.DataAccess.Entities
+{
+    public partial class Project2JAGVContext : DbContext
+    {
+        public Project2JAGVContext()
+        {
+
+        }
+
+        public Project2JAGVContext(DbContextOptions<Project2JAGVContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Logins> Logins { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Pizzas> Pizzas { get; set; }
+        public virtual DbSet<PizzaIngredients> PizzaIngredients { get; set; }
+        public virtual DbSet<Ingredients> Ingredients { get; set; }
+        public virtual DbSet<IngredientTypes> IngredientTypes { get; set; }
+
+
+    }
+}
