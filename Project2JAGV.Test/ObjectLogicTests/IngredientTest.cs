ba270cbc9b229d0f2d669f5b13ef55792ac64c1a
Victoria de la Guardia <victoriadelaguardia@yahoo.com>
2019-10-31 13:35:43 -0500

    added ingredient tests

---
+++ Project2JAGV.Test/ObjectLogicTests/IngredientTest.cs
@@ -0,0 +1,36 @@
+﻿using Project2JAGV.ObjectLogic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Project2JAGV.Test.ObjectLogicTests
+{
+    public class IngredientTest
+    {
+        [Fact]
+        public void Ingredient_Id_Test()
+        {
+            Assert.Throws<ArgumentException>(() => new Ingredient { Id = -1 });
+
+        }
+
+        [Fact]
+        public void Ingredient_Type_Id_Test()
+        {
+            Assert.Throws<ArgumentException>(() => new Ingredient { TypeId = -1 });
+        }
+
+        [Fact]
+        public void Ingredient_Name_Empty()
+        {
+            Assert.Throws<ArgumentException>(() => new Ingredient { Name = "" });
+        }
+
+        [Fact]
+        public void Ingredient_Price_Test()
+        {
+            Assert.Throws<ArgumentException>(() => new Ingredient { Price = -1 });
+        }
+    }
+}
