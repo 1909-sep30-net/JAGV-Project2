5fc4e344e6377b4113acb04b22d8f9228b70de86
Victoria de la Guardia <victoriadelaguardia@yahoo.com>
2019-11-05 20:16:02 -0600

    added mapper test, some user controller functions, models for controllers

---
+++ Project2JAGV.Api/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using Project2JAGV.ObjectLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2JAGV.Api.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+
+        public string UserPassword { get; set; }
+
+        public int UserId { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public UserType UserType { get; set; }
+    }
+}
---
+++ Project2JAGV.Api/Models/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2JAGV.Api.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int DelivererId { get; set; }
+
+        public bool Delivered { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
